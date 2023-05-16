2884694f491fc8c6d6d414f4ac1d4e825a8cd6ab
XDflight <ridge0802@126.com>
2023-05-16 21:11:25 +0800

    Add security.HashTool

--- Client/src/security/HashTool.java
+++ Client/src/security/HashTool.java
@@ -5,6 +5,14 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashTool {
+    /*
+     * @public:     generate
+     * @note:       This method can be used to calculate the hash value of a string.
+     *              The hash algorithm implemented is secure enough to store private and sensitive messages.
+     *              The hash value is so big so that it is returned as a string instead of a number.
+     * @param:      [(String) msg] The message to be hashed.
+     * @retval:     [String] The hash value for the message.
+     */
     public static String generate(String msg) {
         MessageDigest hashInst = getHashInst();
         if (hashInst == null)
@@ -14,6 +22,11 @@ public class HashTool {
         ).toString(16).toUpperCase();
     }
 
+    /*
+     * @private:    getHastInst
+     * @note:       This method can be used to get a hashing-instance from the system JDK.
+     * @retval:     [MessageDigest] A system-provide hashing instance.
+     */
     private static MessageDigest getHashInst() {
         try {
             return MessageDigest.getInstance("MD5");
