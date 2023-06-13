e1f266259128379c9a4ccf596bdbe1287b79ab49
XDflight <ridge0802@126.com>
2023-06-13 13:48:05 +0800

    Merge different versions

--- Client/src/commandNodes/CommandNode.java
+++ Client/src/commandNodes/CommandNode.java
@@ -3,6 +3,7 @@ package commandNodes;
 import security.LoginStatus;
 import security.OperatorLevel;
 import server.DataManager;
+import server.structs.AccountData;
 import server.structs.annotations.LoginRequired;
 import server.structs.annotations.RegisterRequired;
 import util.Logger;
@@ -24,27 +25,31 @@ public class CommandNode {
     private Consumer<Context> executable;
     private Predicate<LoginStatus> hasPower;
 
-    Map<String, CommandNode> children=new HashMap<>();
-    boolean hasFork=false;
-    boolean isParameterStarter=false;
+    Map<String, CommandNode> children = new HashMap<>();
+    boolean hasFork = false;
+    boolean isParameterStarter = false;
     String nextParameter;
 
-    Logger LOGGER=new Logger();
+    Logger LOGGER = new Logger();
 
     public boolean hasFork() {
         return hasFork;
     }
+
     public boolean isEnd() {
         return !hasFork;
     }
-    public String toStringTop(){
-        return name+"\n"+children.toString();
+
+    public String toStringTop() {
+        return name + "\n" + children.toString();
     }
-    public String toString(){
+
+    public String toString() {
         return children.toString();
     }
-    public static CommandNode makeAccountCommands(DataManager dataManager, Class<?> dataType, boolean loginOrRegister){
-        String prefix=loginOrRegister?"login":"register";
+
+    public static CommandNode makeAccountCommands(DataManager dataManager, Class<?> dataType, boolean loginOrRegister) {
+        String prefix = loginOrRegister ? "login" : "register";
         CommandNode commandNode = new CommandNodeFork(
                 prefix
         );
@@ -57,49 +62,51 @@ public class CommandNode {
         bridgeCommandNode.then(subCommandNode);
         commandNode.then(bridgeCommandNode);
 
-        ArrayList<Field> vars=ReflectHelper.getFields(dataType);
-        for (int i = vars.size()-1; i >=0; i--) {
-            Field var=vars.get(i);
-            boolean addVarToParams=false;
-            if(var.isAnnotationPresent(LoginRequired.class)&&loginOrRegister){
-                addVarToParams=true;
+        ArrayList<Field> vars = ReflectHelper.getFields(dataType);
+        for (int i = vars.size() - 1; i >= 0; i--) {
+            Field var = vars.get(i);
+            boolean addVarToParams = false;
+            if (var.isAnnotationPresent(LoginRequired.class) && loginOrRegister) {
+                addVarToParams = true;
             }
-            if(var.isAnnotationPresent(RegisterRequired.class)&&!loginOrRegister){
-                addVarToParams=true;
-            };
-            if(!addVarToParams){
+            if (var.isAnnotationPresent(RegisterRequired.class) && !loginOrRegister) {
+                addVarToParams = true;
+            }
+            ;
+            if (!addVarToParams) {
                 vars.remove(i);
             }
         }
-        subCommandNode.consumeVars(dataManager, dataType,vars,loginOrRegister);
+        subCommandNode.consumeVars(dataManager, dataType, vars, loginOrRegister);
         return commandNode;
     }
-    public CommandNode consumeVars(DataManager dataManager, Class<?> dataType, ArrayList<Field> vars, boolean loginOrRegister){
-        if(vars.size()<=0){
+
+    public CommandNode consumeVars(DataManager dataManager, Class<?> dataType, ArrayList<Field> vars, boolean loginOrRegister) {
+        if (vars.size() <= 0) {
             this.end(
                     (context) -> {
-                        Object entry=fromParam(dataType,context.parameters);
-                        if(loginOrRegister){
-                            boolean hasEntry=dataManager.canLogin((DataClass) entry);
-                            if(!LoginStatus.hasAccount){
-                                if(hasEntry){
+                        Object entry = fromParam(dataType, context.parameters);
+                        if (loginOrRegister) {
+                            boolean hasEntry = dataManager.canLogin((DataClass) entry);
+                            if (!LoginStatus.loggedIn()) {
+                                if (hasEntry) {
                                     LoginStatus.setPermissionLevel(1);
-                                    LoginStatus.setHasAccount(true);
+                                    LoginStatus.setUname(((AccountData) entry).userName);
                                     System.out.println("Login Success, Access Granted");
-                                }else{
+                                } else {
                                     System.out.println("Login Failed");
                                 }
-                            }else{
+                            } else {
                                 System.out.println("Already logged into an account!");
                             }
-                        }else{
+                        } else {
                             dataManager.addEntry((DataClass) entry);
                             System.out.println("added object");
                         }
                     },
                     0
             );
-        }else{
+        } else {
 
             CommandNode commandNode = new CommandNodeInput(
                     vars.get(0).getName(),
@@ -107,71 +114,78 @@ public class CommandNode {
             );
 
             vars.remove(0);
-            this.then(commandNode.consumeVars(dataManager,dataType,vars,loginOrRegister));
+            this.then(commandNode.consumeVars(dataManager, dataType, vars, loginOrRegister));
         }
         return this;
     }
+
     public CommandNode then(CommandNode in) {
-        if(in instanceof CommandNodeInput || in instanceof CommandNodeTags){
-            if(isParameterStarter){
+        if (in instanceof CommandNodeInput || in instanceof CommandNodeTags) {
+            if (isParameterStarter) {
                 try {
                     throw new Exception("Multiple Parameter Fork, unable to handle");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }else{
-                nextParameter=in.name;
-                isParameterStarter=true;
+            } else {
+                nextParameter = in.name;
+                isParameterStarter = true;
             }
         }
-        if(children.containsKey(in.name)){
-            for (String key:
-                 in.children.keySet()) {
+        if (children.containsKey(in.name)) {
+            for (String key :
+                    in.children.keySet()) {
                 children.get(in.name).then(in.children.get(key));
             }
-        }else{
-            children.put(in.name,in);
+        } else {
+            children.put(in.name, in);
         }
-        hasFork=true;
+        hasFork = true;
         return this;
     }
-    public CommandNode end(Consumer<Context> in){
-        executable=in;
+
+    public CommandNode end(Consumer<Context> in) {
+        executable = in;
         OperatorLevel operatorLevel = OperatorLevel.ADMIN;
-        Predicate<LoginStatus> predicate=(status)-> {
+        Predicate<LoginStatus> predicate = (status) -> {
             return LoginStatus.hasPermissionLevel(3);
         };
-        hasPower=predicate;
+        hasPower = predicate;
         return this;
     }
-    public CommandNode end(Consumer<Context> in, int level){
-        executable=in;
+
+    public CommandNode end(Consumer<Context> in, int level) {
+        executable = in;
         OperatorLevel operatorLevel = OperatorLevel.ADMIN;
-        Predicate<LoginStatus> predicate=(status)-> {
+        Predicate<LoginStatus> predicate = (status) -> {
             return LoginStatus.hasPermissionLevel(level);
         };
-        hasPower=predicate;
+        hasPower = predicate;
         return this;
     }
-    public CommandNode end(Consumer<Context> in, Predicate<LoginStatus> predicate){
-        executable=in;
-        hasPower=predicate;
+
+    public CommandNode end(Consumer<Context> in, Predicate<LoginStatus> predicate) {
+        executable = in;
+        hasPower = predicate;
         return this;
     }
-    public CommandNode progress(String in){
-        if(isParameterStarter){
+
+    public CommandNode progress(String in) {
+        if (isParameterStarter) {
             return progress();
-        }else{
-            return children.containsKey(in)? children.get(in):null;
+        } else {
+            return children.containsKey(in) ? children.get(in) : null;
         }
     }
-    public CommandNode progress(){
+
+    public CommandNode progress() {
         return children.get(nextParameter);
     }
-    public void run(Context params){
-        if(hasPower.test(new LoginStatus())){
+
+    public void run(Context params) {
+        if (hasPower.test(new LoginStatus())) {
             executable.accept(params);
-        }else{
+        } else {
             LOGGER.warn("no permission");
         }
     }
