37578a54a78263f4f136cee5ab0d3626bdfad82a
XDflight <ridge0802@126.com>
2023-06-14 08:51:12 +0800

    Modified DataManager

--- Client/src/server/DataManager.java
+++ Client/src/server/DataManager.java
@@ -147,7 +147,8 @@ public class DataManager {
                         ArrayList<DataClass> refList=new ArrayList<>();
 
                         DataManager dataset=getDataManager(field.getAnnotation(RefList.class).classType());
-                        if(paramVal instanceof String aString){
+                        if(paramVal instanceof String){
+                            String aString = (String) paramVal;
                             String[] refs=aString.split(",");
                             for (String s:
                                     refs) {
@@ -159,7 +160,8 @@ public class DataManager {
                     if(field.isAnnotationPresent(Ref.class)){
 
                         DataManager dataset=getDataManager(field.getAnnotation(Ref.class).classType());
-                        if(paramVal instanceof String aString){
+                        if(paramVal instanceof String){
+                            String aString = (String) paramVal;
                             fieldVal=dataset.getByUUID(aString);
                         }
 
